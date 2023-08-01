24a24a368cce77251eba91bb49e166b6d264695b
inomata137 <inomata-kazuki137@g.ecc.u-tokyo.ac.jp>
2023-08-01 19:08:05 +0900

    update test code

--- template/tests/sample_inputs.rs
+++ template/tests/sample_inputs.rs
@@ -2,46 +2,46 @@ use cli_test_dir::*;
 
 const BIN: &'static str = "./main";
 
-#[test]
-fn sample1() {
+fn test(input: &str, answer: &str) {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
-3 1 2
-6 1 1
-"#)
+        .output_with_stdin(input)
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "Yes\n");
+    assert_eq!(output.stdout_str(), answer);
     assert!(output.stderr_str().is_empty());
+}
+
+mod test {
+use crate::test;
+
+#[test]
+fn sample1() {
+test(
+r#"2
+3 1 2
+6 1 1
+"#,
+"Yes\n");
 }
 
 #[test]
 fn sample2() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(r#"1
+test(
+r#"1
 2 100 100
-"#)
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
-    assert!(output.stderr_str().is_empty());
+"#,
+"No\n");
 }
 
 #[test]
 fn sample3() {
-    let testdir = TestDir::new(BIN, "");
-    let output = testdir
-        .cmd()
-        .output_with_stdin(r#"2
+test(
+r#"2
 5 1 1
 100 1 1
-"#)
-        .tee_output()
-        .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
-    assert!(output.stderr_str().is_empty());
+"#,
+"No\n");
+}
 }
